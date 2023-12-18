767c81d39a545cad443d83d7ac4dfbc2eeb18eda
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-12-18 16:59:08 +0000

    2023 Day 17 (IMPROVE CODE, STILL DOESN'T WORK)

--- 2023/17/part1-FAILED.py
+++ 2023/17/part1-FAILED.py
@@ -1,7 +1,41 @@
+import heapq
 import networkx as nx
 
 with open("input.txt", "r", encoding="utf8") as f:
     lines = [[int(i) for i in line] for line in f.read().splitlines()]
+
+
+def dijkstra(graph, start, end):
+    queue = [(0, start)]
+    distances = {node: float('infinity') for node in graph.nodes}
+    distances[start] = 0
+    paths = {start: []}
+
+    while queue:
+        (distance, current) = heapq.heappop(queue)
+        if current == end:
+            return paths[end] + [end]
+        for neighbor in graph.neighbors(current):
+            if not check_restriction(current, neighbor, paths[current]):
+                continue
+            old_distance = distances[neighbor]
+            new_distance = distance + graph.edges[current, neighbor]['weight']
+            if new_distance < old_distance:
+                distances[neighbor] = new_distance
+                paths[neighbor] = paths[current] + [current]
+                heapq.heappush(queue, (new_distance, neighbor))
+    return None
+
+
+def check_restriction(current, neighbour, path):
+    if len(path) >= 2:
+        four = (path[-2], path[-1], current, neighbour)
+        if len(set([node[0] for node in four])) == 1 and tuple(sorted(four, key=lambda node: node[1])) == four:
+            return False
+        if len(set([node[1] for node in four])) == 1 and tuple(sorted(four, key=lambda node: node[0])) == four:
+            return False
+    return True
+
 
 G = nx.Graph()
 for i, line in enumerate(lines):
@@ -17,3 +51,7 @@ for i, line in enumerate(lines):
             G.add_edge((i, j), (i, j-1), weight=lines[i][j-1])
         if j < len(lines) - 1:
             G.add_edge((i, j), (i, j+1), weight=lines[i][j+1])
+
+path = dijkstra(G, (0, 0), (len(lines) - 1, len(lines[0]) - 1))
+print(path)
+print(sum([lines[node[0]][node[1]] for node in path]))
