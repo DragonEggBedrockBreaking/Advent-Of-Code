af3b85cd9a91ca8d01c3f943363a3f379f679cd6
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-12-20 07:05:34 +0000

    2023 Day 19 (Part 2 improved, still very much broken)

--- 2023/19/part2-BROKEN.py
+++ 2023/19/part2-BROKEN.py
@@ -1,5 +1,5 @@
-import re
 import functools
+import re
 
 with open("input.txt", "r", encoding="utf8") as f:
     first, second = f.read().split("\n\n")
@@ -9,54 +9,129 @@ data = {}
 for line in first:
     match = re.search(r'(.*){(.*)}', line)
     data[match.group(1)] = tuple(str(match.group(2)).split(","))
-
-As = []
-for i, value in enumerate(data.values()):
-    for part in str(value).split(","):
-        if "A" in part:
-            As.append(i)
-
-finals = set()
+ranges = []
 
 
-@functools.cache
-def find_in(current, xrange=frozenset(range(1, 4001)), mrange=frozenset(range(1, 4001)),
-            arange=frozenset(range(1, 4001)), srange=frozenset(range(1, 4001))):
-    global finals
-    if current == "in":
-        finals = finals & [(x, m, a, s) for x in xrange for m in mrange for a in arange for s in srange]
-        return
-    for key, val in data.items():
-        for pt in val:
-            if current == pt:
-                find_in(key, xrange, mrange, arange, srange)
-            elif current in pt:
-                newx, newm, newa, news = xrange, mrange, arange, srange
-                if ">" in pt:
-                    one, two = pt.split(":")[0].split(">")
-                    match one:
+def traverse(current="in", x=(1, 4000), m=(1, 4000), a=(1, 4000), s=(1, 4000)):
+    global ranges
+    for condition in data[current]:
+        if condition == "A":
+            ranges.append((x, m, a, s))
+        elif condition != "R":
+            if ":" not in condition:
+                traverse(condition, x, m, a, s)
+            else:
+                cond, dest = condition.split(":")
+                if ">" in cond:
+                    var, num = cond.split(">")
+                    match var:
                         case "x":
-                            newx = frozenset(xrange | set(range(int(two) + 1, 4001)))
+                            if int(num) < x[0]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) < x[1]:
+                                traverse(current, (int(num) + 1, x[1]), m, a, s)
+                                traverse(current, (x[0], int(num)), m, a, s)
                         case "m":
-                            newm = frozenset(mrange | set(range(int(two) + 1, 4001)))
+                            if int(num) < m[0]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) < m[1]:
+                                traverse(current, x, (int(num) + 1, m[1]), a, s)
+                                traverse(current, x, (m[0], int(num)), a, s)
                         case "a":
-                            newa = frozenset(arange | set(range(int(two) + 1, 4001)))
+                            if int(num) < a[0]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) < a[1]:
+                                traverse(current, x, m, (int(num) + 1, a[1]), s)
+                                traverse(current, x, m, (a[0], int(num)), s)
                         case "s":
-                            news = frozenset(srange | set(range(int(two) + 1, 4001)))
-                elif "<" in pt:
-                    one, two = pt.split(":")[0].split("<")
-                    match one:
+                            if int(num) < s[0]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) < s[1]:
+                                traverse(current, x, m, a, (int(num) + 1, s[1]))
+                                traverse(current, x, m, a, (s[0], int(num)))
+                elif "<" in cond:
+                    var, num = cond.split("<")
+                    match var:
                         case "x":
-                            newx = frozenset(xrange | set(range(1, int(two))))
+                            if int(num) > x[1]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) > x[0]:
+                                traverse(current, (x[0], int(num) - 1), m, a, s)
+                                traverse(current, (int(num), x[1]), m, a, s)
                         case "m":
-                            newm = frozenset(mrange | set(range(1, int(two))))
+                            if int(num) > m[1]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) > m[0]:
+                                traverse(current, x, (m[0], int(num) - 1), a, s)
+                                traverse(current, x, (int(num), m[1]), a, s)
                         case "a":
-                            newa = frozenset(arange | set(range(1, int(two))))
+                            if int(num) > a[1]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) > a[0]:
+                                traverse(current, x, m, (a[0], int(num) - 1), s)
+                                traverse(current, x, m, (int(num), a[1]), s)
                         case "s":
-                            news = frozenset(srange | set(range(1, int(two))))
-                find_in(key, newx, newm, newa, news)
+                            if int(num) > s[1]:
+                                if dest == "A":
+                                    ranges.append((x, m, a, s))
+                                elif dest != "R":
+                                    traverse(dest, x, m, a, s)
+                            elif int(num) > s[0]:
+                                traverse(current, x, m, a, (s[0], int(num) - 1))
+                                traverse(current, x, m, a, (int(num), s[1]))
 
 
-for i in As:
-    print(i)
-    find_in(list(data.keys())[i])
+traverse()
+final = 0
+
+
+def sum_arithmetic_series(start, end):
+    num_terms = end - start + 1
+    return num_terms * (start + end) // 2
+
+
+def total_sum_of_combinations(rngs):
+    sum_1 = sum_arithmetic_series(rngs[0][0], rngs[0][1])
+    sum_2 = sum_arithmetic_series(rngs[1][0], rngs[1][1])
+    sum_3 = sum_arithmetic_series(rngs[2][0], rngs[2][1])
+    sum_4 = sum_arithmetic_series(rngs[3][0], rngs[3][1])
+
+    total_sum = (
+            sum_1 * (rngs[1][1] - rngs[1][0] + 1) * (rngs[2][1] - rngs[2][0] + 1) * (
+            rngs[3][1] - rngs[3][0] + 1) +
+            sum_2 * (rngs[0][1] - rngs[0][0] + 1) * (rngs[2][1] - rngs[2][0] + 1) * (
+                    rngs[3][1] - rngs[3][0] + 1) +
+            sum_3 * (rngs[0][1] - rngs[0][0] + 1) * (rngs[1][1] - rngs[1][0] + 1) * (
+                    rngs[3][1] - rngs[3][0] + 1) +
+            sum_4 * (rngs[0][1] - rngs[0][0] + 1) * (rngs[1][1] - rngs[1][0] + 1) * (
+                    rngs[2][1] - rngs[2][0] + 1)
+    )
+
+    return total_sum
+
+
+total = 0
+for range in ranges:
+    total += total_sum_of_combinations(range)
+print(total)
